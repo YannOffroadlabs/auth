9af25490a69db1a4a92b733b0e2080b999c0b954
Yann <yann@offroadlabs.com>
2024-11-05 10:00:14 +0100

    fin de session

--- app/(auth)/training/page.js
+++ app/(auth)/training/page.js
@@ -1,6 +1,13 @@
 import { getTrainings } from '@/lib/training';
+import { verifyAuth } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
 export default async function TrainingPage() {
+  const { user, session } = await verifyAuth();
+
+  if (!user) {
+    return redirect('/');
+  }
   const trainingSessions = getTrainings();
 
   return (
